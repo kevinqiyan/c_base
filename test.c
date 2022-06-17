c532325a792c60b70098c0f943327578a72ae4c2
kevinqiyan <kevin1025253@gmail.com>
2022-06-17 21:08:48 +0800

    C语言基础

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define PI 3.14
+int main(void)
+{
+    /* code */
+    int a1 = 1,b,c;
+    /* a1 * PI;
+    b + PI;
+    c / PI; */
+    bool a = false;
+    printf("a = %d\n", a);
+    printf("hello world\n");
+    printf("p = %d\n", PI);
+    return 0;
+}
+
